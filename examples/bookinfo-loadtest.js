545e322ffc8ef9313fdc4694b1bc31f13b5c4579
Angelo Marchese <marchese.angelo1@gmail>
2022-09-23 09:26:23 +0200

    Add examples folder

---
+++ examples/bookinfo-loadtest.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '5m',
+};
+
+export default function () {
+  http.get('http://172.19.0.2:30000/productpage');
+  http.get('http://172.19.0.4:30000/productpage');
+  http.get('http://172.19.0.5:30000/productpage');
+  sleep(1);
+}
